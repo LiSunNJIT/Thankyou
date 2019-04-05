a1d9c4711b131e433d3a607827ebf01b6bee7405
li <ls447@njit.edu>
2019-04-05 15:14:30 -0400

    revise the content

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,6 +7,11 @@
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
         <div class="container">
+
+
+            <br>
+            <br>
+            <br>
             <h1 class="display-3">Hello, world!</h1>
             <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
             <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
@@ -18,12 +23,12 @@
         <div class="row">
             <div class="col-md-4">
                 <h2>Home</h2>
-                <p>First page </p>
+                <p>First page Home page</p>
                 <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
             </div>
             <div class="col-md-4">
                 <h2>About</h2>
-                <p>Second page </p>
+                <p>Second page About page</p>
                 <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
             </div>
             <div class="col-md-4">
